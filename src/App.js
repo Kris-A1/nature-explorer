0ac019018a70a030c2fde15a0ca22dbca74bd056
Kris-A1 <almazidoukristina@gmail.com>
2025-09-10 17:20:54 +0300

    Add React starter file App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Nature Explorer</h1>
+      <p>Welcome to the Nature Explorer React app!</p>
+    </div>
+  );
+}
+
+export default App;
